b2bb5e6881aa10210f98a22f0abe0d04bdd08353
Andraž Oset <andraz.oset@student.um.si>
2024-10-24 12:45:18 +0200

    Delete frontend/your-voice directory

--- frontend/your-voice/src/interfaces/User.ts
+++
@@ -1,5 +0,0 @@
-export interface User {
-    id: string;           // Change this to match what you're expecting
-    name: string;         // Change this to match what you're expecting
-    // Add any other properties that are necessary
-}
