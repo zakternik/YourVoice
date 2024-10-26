0951911e7a5e378488e9570112863f8ea6fef7de
Andraz Oset <andraz@zaki.si>
2024-10-26 02:46:38 +0200

    feat: update footer and improve styling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import '../styles.scss';
 
 const Footer: React.FC = () => {
   return (
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div className="col-md-6 d-flex align-items-center">
-          <span className="mb-5 mb-md-0 text-muted">
-            &copy; Projekt Your Voice, Fakulteta za elektrotehniko,
-            računalništvo in informatiko Maribor
+      <footer className="footer d-flex flex-wrap justify-content-between align-items-start py-3 my-4 border-top">
+        <div className="col-md-6 d-flex flex-column align-items-start">
+          <span className="footer-text mb-1">
+            v0.0.1 Beta © 2024 by Oset, Ternik, Vučetić
+          </span>
+          <span className="footer-text">
+            Projekt Your Voice, Fakulteta za elektrotehniko, računalništvo in
+            informatiko Maribor
           </span>
         </div>
 
@@ -62,7 +66,7 @@ const Footer: React.FC = () => {
                 className="bi bi-facebook"
                 viewBox="0 0 16 16"
               >
-                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.62l-.419 2.321h-2.201v5.625C13.074 15.397 16 12.067 16 8.049z" />
               </svg>
             </a>
           </li>
