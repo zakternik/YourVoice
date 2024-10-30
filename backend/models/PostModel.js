0d8c58e0cc3d54d1a90e083d39f49b7829ddfa1f
sinisaV <sinisa.vucetic@student.um.si>
2024-10-30 17:55:45 +0100

    Updated PostModel

--- backend/models/PostModel.js
+++ backend/models/PostModel.js
@@ -8,9 +8,10 @@ var PostSchema = new Schema({
     author: { 
         type: Schema.Types.ObjectId, 
         ref: 'user', 
-        required: true },
+        required: false },
     upvotes: { type: Number, default: 0 },
     downvotes: { type: Number, default: 0 },
+    numComments: { type: Number, default: 0 },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
