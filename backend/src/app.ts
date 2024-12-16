33783397a2fc21fb7585e1384fe6274151afca1c
ALjaz Kokol <aljaz.kokol@easy.bi>
2024-12-16 18:06:06 +0100

    feature: [YOUR-9] user endpoints init. refactor

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,13 +3,15 @@ import 'express-async-errors';
 
 import { cors, globalErrorHandler } from './_common/middlewares/index.js';
 import postRoutes from './post/post.routes.js';
+import userRoutes from './user/user.routes.js';
 
 const app = express();
 
 app.use(express.json({ limit: '5mb' }));
 app.use(cors);
 
-app.use('/post', postRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/user', userRoutes);
 app.use(globalErrorHandler);
 
 export default app;
--- backend/src/user/user.routes.ts
+++ backend/src/user/user.routes.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 
-import UserController from '../controllers/UserController.js';
-
 const router = Router();
 
 router.get('/', UserController.list);
